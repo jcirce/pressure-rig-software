8e9490f6f517a0a6a7d07df0a0cca26c3982441d
jcirce <jett@circe.com>
2022-12-01 21:45:07 -0500

    simplified setup class, makes dir successfully from input

--- src/python/util/setup.py
+++ src/python/util/setup.py
@@ -1,10 +1,10 @@
 
 
 class Tube:
-    def __init__(self, name, number, test):
-        self.name = name
-        self.number = number
-        self.test = test
+    def __init__(self):
+        self.name = input("enter tube name (ex: D20K or S45M): ")
+        self.number = input("enter tube number: ")
+        self.test = input("enter test number: ")
 
     def __str__(self):
         return f"{self.name}{self.number}_{self.test}"
--- src/python/util/test.py
+++ src/python/util/test.py
@@ -5,7 +5,6 @@ import os
 from setup import Tube
 
 c = CommsController() 
-
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
@@ -16,9 +15,13 @@ counter = 0 #test only runs for set pressure amount
 
 bit = np.array([255, 160, 113,  87,  69,  56,  47,  40,   34,   29,   25])
 psi = np.array([2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.1])
+kai = np.size(bit) - 1 #times want to take photos
+
+tube = Tube()
+print(tube)
 
 parent_dir = "/Users/student/desktop/pressure-rig-software/src/python/util"
-dir = str("tube") #makes sure its a string
+dir = str(tube) #makes sure its a string
 path = os.path.join(parent_dir, dir)
 os.mkdir(path) #makes dir for photos to go in
 print("dir made for {}".format(dir))
@@ -37,7 +40,14 @@ while True:
     #print(k) #prints int of key pressed
 
     if k == 32:
-        print("up pressure")
+        #print("up pressure")
+        print("sending {} bit".format(bit[counter]))
+
+        c.append_command(b"R0%\n")
+        s = f"S1,A{bit[counter]}%\n"
+        c.append_command(bytes(s, 'UTF-8'))
+        a = c.responseList.get()
+        print("sent command {}".format(a))
 
     if k == 109:
         #print("take photo")
@@ -46,10 +56,15 @@ while True:
         print("{} written".format(img_name))
         counter += 1
 
+        if counter > kai:
+                print("end of test")
+                break
+
     if k == 113:
         break
 
 
 cap.release()
 cv2.destroyAllWindows()
+exit()
 
