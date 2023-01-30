14039277dc94f21f49eda91008a8deb1ca2ce244
poltergeistjoya <joyaranidebi@gmail.com>
2023-01-30 13:30:23 -0500

    contour looks good, need to crop photo and do checkerboard

--- linedet/linedet.py
+++ linedet/linedet.py
@@ -2,37 +2,46 @@
 
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+import os 
 
-testfold = '../data/D20K5_1'
-testimpath = '../data/D20K5_1/D20K5_1_psi_1.7.png'
+from matplotlib import pyplot as plt
 
 #crop
 def crop(img):
-    cropimg = img[0:280, 0:360]
+    cropimg = img[500:, 0:360]
     return cropimg
 
-img = crop(cv2.imread(testimpath, cv2.IMREAD_UNCHANGED))
-img_grey = crop(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+testfold = 'data/D20K5_withgrid1/'
+for path in os.listdir(testfold):
+
+    testimpath = testfold+path
+    print(testimpath)
+
+    img = cv2.imread(testimpath, cv2.IMREAD_UNCHANGED)
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-ret, mask = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-mask2 = cv2.adaptiveThreshold(img_grey, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, \
-    cv2.THRESH_BINARY, 11, 2)
+    ret, mask = cv2.threshold(img2, 127, 255, cv2.THRESH_BINARY)
 
-contours, hierachy = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierachy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.drawContours(img, contours, -1, (0,255,0),3)
-cv2.imshow('Contours', img)
-cv2.waitKey(0)
+    plt.imshow(mask)
+    plt.show()
+    plt.waitforbuttonpress
 
-images = [mask, mask2]
+    cv2.drawContours(img, contours, -1, (0,255,0),3)
+    cv2.imshow('Contours', img)
+    cv2.waitKey(0)
+    #break
 
-# for i in range(2):
-#     plt.subplot(2, 2, i+1)
-#     plt.imshow(images[i])
-# plt.show()
+    # images = [mask]
 
-#try this next 
-#https://www.geeksforgeeks.org/find-and-draw-contours-using-opencv-python/
-#look into canny, sorel, fill area, thresholds for contours
+    # for i in range(2):
+    #     plt.subplot(2, 2, i+1)
+    #     plt.imshow(images[i])
+    # plt.show()
+
+    # try this next 
+    # https://www.geeksforgeeks.org/find-and-draw-contours-using-opencv-python/
+    # look into canny, sorel, fill area, thresholds for contours
