baaaf28217bcaa7b31440b7d368f6605cdcc6321
jcirce <jett@circe.com>
2023-02-11 19:22:26 -0500

    working version

--- src/python/util/testboth.py
+++ src/python/util/testboth.py
@@ -7,7 +7,7 @@ from setup import Tube
 
 c = CommsController() 
 cap = cv2.VideoCapture(0)
-l = LoadCell()
+# l = LoadCell()
 
 if not cap.isOpened():
     print("cannot open camera")
@@ -16,11 +16,10 @@ if not cap.isOpened():
 counter = 0 #test only runs for set pressure amount
 photocounter = 0 
 
-#these values from jan/11 map
-bit = np.array([255,    88,   61,   41,   30,   22,   16,   12,   11,   10,     9,     6,     4,     2,     1,     0]) 
-psi = np.array([1.7,   3.3,  4.4,  5.9,  7.2,  8.7, 10.3, 11.6, 12.0, 12.5,  13.0,  14.7,  16.0,  17.7,  18.8,  19.9])
-kpa = np.array([11.0, 22.8, 30.3, 40.7, 49.6, 60.0, 71.0, 80.0, 82.7, 86.2,  89.6, 101.4, 110.3, 122.0, 129.6, 137.2])
-
+#these values from isaiah's map 0-20psi
+bit = np.array([255,    88,   61,   41,   30,   22,   20,   18,   16,   14,   12,   11,   10,     9,     6,     4,     2,     1, 0]) 
+psi = np.array([1.7,   3.3,  4.4,  5.9,  7.2,  8.7,  9.2,  9.7, 10.3, 10.9, 11.6, 12.0, 12.5,  13.0,  14.7,  16.0,  17.7,  18.8, 19.9])
+kpa = np.array([11.0, 22.8, 30.3, 40.7, 49.6, 60.0, 63.4, 66.9, 71.0, 75.1, 80.0, 82.7, 86.2,  89.6, 101.4, 110.3, 122.0, 129.6, 137.2])
 
 
 kai = np.size(bit) - 1 #times want to take photos, twice number of bit commands we have, 15
@@ -30,12 +29,13 @@ up = True #tracking if pressure increasing or not
 tube = Tube() #asks for name, number, test number
 print(tube)
 
-parent_dir = "/Users/student/desktop/pressure-rig-software/data-good"
+parent_dir = "/Users/student/desktop/pressure-rig-software/data-best"
 dir = str(tube) #makes sure its a string
 path = os.path.join(parent_dir, dir)
-os.mkdir(path) #makes dir for photos to go in
+os.mkdir(path) #makes dir for photos to go in 
 print("dir made for {}".format(dir))
-# print("path")
+
+# print("path") 
 # print(path)
 
 f = open("force_{}.txt".format(tube), "a")
@@ -57,31 +57,31 @@ while True:
     #print(k) #prints int of key pressed
 
     if k == 32: #space pressed
-        #print("sending {} bit".format(bit[counter]))
+        #print("sending {} psi".format(psi[counter]))
         c.append_command(b"R0%\n")
         s = f"S1,A{bit[counter]}%\n" 
         c.append_command(bytes(s, 'UTF-8'))
         a = c.responseList.get()
         #print("sent command {}".format(a))
-        print("press f to take force reading")
-        # print("press m to take photo")
-
-    if k == 102: #f pressed
-        # force = l.get_reading()
-        # print("force is = {}".format(force))
-        force = np.empty([10])
-        
-        for i in range(10):
-            force[i] = l.get_reading()
-
-        avg_force = np.average(force)
-        gram = round(0.000749711*avg_force + 0.75729)
-
-        print("force is = {} g".format(gram))
-        f.write("{} raw, {} g \n".format(avg_force, gram))
-        #print("recorded force reading")
-    
+        # print("press f to take force reading")
         print("press m to take photo")
+
+    # if k == 102: #f pressed
+    #     # force = l.get_reading()
+    #     # print("force is = {}".format(force))
+    #     force = np.empty([10])
+        
+    #     for i in range(10):
+    #         force[i] = l.get_reading()
+
+    #     avg_force = np.average(force)
+    #     gram = round(0.000749711*avg_force + 0.75729)
+
+    #     print("force is = {} g".format(gram))
+    #     f.write("{} kpa, {} raw, {} g \n".format(kpa[counter], avg_force, gram))
+    #     #print("recorded force reading")
+    
+    #     print("press m to take photo")
 
 
     if k == 109: #m pressed
